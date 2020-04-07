4c7e262f522238fffc28c77c0642636ca4f08f89
Carlos Daniel <danahpa@gmail.com>
2020-04-06 20:45:46 -0500

    start project and basic move

---
+++ Assets/GameManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+    menu,
+    newCube,
+    startGame
+}
+
+public class GameManager : MonoBehaviour
+{
+    public float limitLeft, limitRight;
+    public float speed = 3.0f;
+    public float spacingPiecesFactor= 0.5f;
+    public GameState currentGameState = GameState.startGame;
+    public static GameManager sharedInstance;
+    public List<pieceScript> myPieces= new List<pieceScript>();
+    public List<pieceScript> tower= new List<pieceScript>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        //piece= Instantiate<cube>
+    }
+    private void Awake()
+    {
+        if (sharedInstance == null)
+            sharedInstance = this;
+        //yo soy la instancia compartida y el unico que va a gobernar.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentGameState == GameState.startGame && Input.GetMouseButtonDown(0))
+        {
+            CreateNewCube();
+        }
+
+        
+
+    }
+
+    public void CreateNewCube (){
+        Debug.Log("neeeew");
+        SetGameState(GameState.newCube);
+}
+
+    private void SetGameState(GameState newGameState)
+    {
+        if (newGameState == GameState.newCube)
+        {
+            pieceScript newPiece;
+            newPiece = Instantiate(myPieces[0]);
+            pieceScript lastPiece = tower[tower.Count-1];
+            lastPiece.GetComponent<pieceScript>().newPiece = true;
+            newPiece.transform.position = new Vector3(limitLeft, lastPiece.transform.position.y + spacingPiecesFactor, lastPiece.transform.position.z);
+            tower.Add(newPiece);
+
+            //TODO colocar la logica del menu
+        }
+        this.currentGameState = newGameState;
+    }
+    }
---
+++ Assets/pieceScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pieceScript : MonoBehaviour
+{
+
+    float speed;
+    bool goingRight;
+    bool stopMoving;
+    public bool newPiece;
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        newPiece = false;
+        goingRight = true;
+        stopMoving = false;
+        //si la torre tiene mas de una pieza le ponemos su velocidad
+        if (GameManager.sharedInstance.tower.Count > 1)
+            speed = GameManager.sharedInstance.speed;
+        else
+            speed = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (newPiece == true) 
+        {
+            stopMoving = true;
+            rb.useGravity = true;
+            GameManager.sharedInstance.currentGameState = GameState.startGame;
+           
+        }
+
+        if (!stopMoving)
+        {
+            if (transform.position.x > GameManager.sharedInstance.limitRight)
+            {
+                goingRight = false;
+            }
+            if (transform.position.x < GameManager.sharedInstance.limitLeft)
+            {
+                goingRight = true;
+            }
+
+            if (goingRight)
+            {
+                transform.position = Vector3.Lerp(transform.position, new Vector3(GameManager.sharedInstance.limitRight + 1, transform.position.y, transform.position.z), Time.deltaTime * speed);
+
+            }
+            else
+            {
+                transform.position = Vector3.Lerp(transform.position, new Vector3(GameManager.sharedInstance.limitLeft - 1, transform.position.y, transform.position.z), Time.deltaTime * speed);
+
+            }
+        }
+    }
+}
